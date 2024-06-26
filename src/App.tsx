c986857fc995e062b8544528fd471b334d1738d5
Vedbhanushali <19ce005@charusat.edu.in>
2024-06-26 14:00:25 +0530

    added Gasp and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -10,9 +9,7 @@ function App() {
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
